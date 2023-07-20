18670c538efff44b20f012a63b8dfd8c85b494a8
kotwiceal <kotvitskyaya@gmail.com>
2023-07-20 13:13:57 +0300

    Create PIV interface section. Following modifications: tcp server and its handler functions to process data and visualize results as well as a table to store and change parameters of processing have been implemented.

--- applab_exported.m
+++ applab_exported.m
@@ -2,7 +2,53 @@ classdef applab_exported < matlab.apps.AppBase
 
     % Properties that correspond to app components
     properties (Access = public)
-        UIFigure  matlab.ui.Figure
+        UIFigure     matlab.ui.Figure
+        GridLayout   matlab.ui.container.GridLayout
+        LogTextArea  matlab.ui.control.TextArea
+        TabGroup     matlab.ui.container.TabGroup
+        PIVTab       matlab.ui.container.Tab
+    end
+
+    
+    properties (Access = private)
+        module = struct() % to store imported modules;
+        queue_pool = struct('log', parallel.pool.DataQueue, 'disp', parallel.pool.DataQueue) % to store queue pool
+    end
+    
+    methods (Access = private)
+        % define logger function
+        function log(app, message)
+            % INPUT:
+            %       message - displayed string
+            app.LogTextArea.Value = [app.LogTextArea.Value; strcat(string(datetime), " ", message)];
+            scroll(app.LogTextArea, 'bottom');
+        end
+    end
+    
+
+    % Callbacks that handle component events
+    methods (Access = private)
+
+        % Code that executes after component creation
+        function startupFcn(app)
+            clc
+            % initialize functions to workers
+            afterEach(app.queue_pool.log, @app.log)
+            afterEach(app.queue_pool.disp, @disp)
+            
+            % create piv interface section
+            app.module.piv = piv(app.PIVTab, app.queue_pool);
+        end
+
+        % Close request function: UIFigure
+        function UIFigureCloseRequest(app, event)
+            % forced deleting TCP server instance
+            try
+                delete(app.module.piv.tcp_server)
+            catch
+            end
+            delete(app)
+        end
     end
 
     % Component initialization
@@ -13,8 +59,32 @@ classdef applab_exported < matlab.apps.AppBase
 
             % Create UIFigure and hide until all components are created
             app.UIFigure = uifigure('Visible', 'off');
-            app.UIFigure.Position = [100 100 640 480];
+            app.UIFigure.Position = [100 100 1191 699];
             app.UIFigure.Name = 'MATLAB App';
+            app.UIFigure.CloseRequestFcn = createCallbackFcn(app, @UIFigureCloseRequest, true);
+            app.UIFigure.WindowStyle = 'alwaysontop';
+
+            % Create GridLayout
+            app.GridLayout = uigridlayout(app.UIFigure);
+            app.GridLayout.ColumnWidth = {'1x'};
+            app.GridLayout.RowHeight = {'1x', '0.2x'};
+            app.GridLayout.ColumnSpacing = 0;
+            app.GridLayout.RowSpacing = 0;
+            app.GridLayout.Padding = [0 0 0 0];
+
+            % Create TabGroup
+            app.TabGroup = uitabgroup(app.GridLayout);
+            app.TabGroup.Layout.Row = 1;
+            app.TabGroup.Layout.Column = 1;
+
+            % Create PIVTab
+            app.PIVTab = uitab(app.TabGroup);
+            app.PIVTab.Title = 'PIV';
+
+            % Create LogTextArea
+            app.LogTextArea = uitextarea(app.GridLayout);
+            app.LogTextArea.Layout.Row = 2;
+            app.LogTextArea.Layout.Column = 1;
 
             % Show the figure after all components are created
             app.UIFigure.Visible = 'on';
@@ -32,6 +102,9 @@ classdef applab_exported < matlab.apps.AppBase
 
             % Register the app with App Designer
             registerApp(app, app.UIFigure)
+
+            % Execute the startup function
+            runStartupFcn(app, @startupFcn)
 
             if nargout == 0
                 clear app
